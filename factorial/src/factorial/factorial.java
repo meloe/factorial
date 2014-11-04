8427ef97850feca328a690b95fd274db26072c59
meloe <melo_e92@hotmail.com>
2014-11-04 17:18:41 +0100

    Versió inicial

---
+++ factorial/src/factorial/factorial.java
@@ -0,0 +1,19 @@
+package factorial;
+
+public class factorial {
+	public static double factorial (double numero) {
+		
+		if (numero==0)
+			return 1;
+		else
+		{
+			double resultat = numero * factorial (numero-1);
+			return resultat;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(factorial(0));
+	}
+
+}
